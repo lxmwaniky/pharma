a312f9ac15ec1232b50a3a7e5d224c49d5f86c4c
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-10-14 23:55:26 +0300

    docs : Documentation for Login.java

--- src/com/pages/Login.java
+++ src/com/pages/Login.java
@@ -1,11 +1,15 @@
 package com.pages;
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatIntelliJLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import com.utils.InputFieldFocusListener;
 
-import javax.swing.UIManager;
 import javax.swing.*;
+/*
+ * Author : Briane Lomoni 168864
+ * Functionality :
+ * - Login form with input fields for Username, Password, User role
+ * - Set Focus Listeners on Username and Password fields
+ * - Handle focus events to clear and set placeholder text in input fields
+ *
+ */
 public class Login{
     private JTextField usernameFormattedTextField;
     private JButton loginButton;
@@ -21,24 +25,16 @@ public class Login{
         setFocusListeners();
     }
 
-    //Getter and Setter Functions
-    public JTextField getUsernameFormattedTextField() {
-        return usernameFormattedTextField;
-    }
-
     //Set focus listeners
-    public void setFocusListeners(){
+    private void setFocusListeners(){
         final InputFieldFocusListener usernameFieldListener = new InputFieldFocusListener(usernameFormattedTextField, "Username...");
         usernameFormattedTextField.addFocusListener(usernameFieldListener);
 
         final InputFieldFocusListener passwordFieldListener = new InputFieldFocusListener(passwordPasswordField, "Password...");
         passwordPasswordField.addFocusListener(passwordFieldListener);
     }
-    public void setUsernameFormattedTextField(JFormattedTextField usernameFormattedTextField) {
-        this.usernameFormattedTextField = usernameFormattedTextField;
-    }
 
-    public JPanel getMainPanel(){
+    public JPanel createMainPanel(){
         return mainPanel;
     }
 }
