140f7c4d2ee940c1cc7afb945de74f353c626f7e
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-10-14 20:39:30 +0300

    Feature : Import FlatLaf theme

--- src/com/pages/Login.java
+++ src/com/pages/Login.java
@@ -1,14 +1,22 @@
 package com.pages;
-
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
 import javax.swing.*;
 public class Login extends JFrame{
     private JFormattedTextField usernameFormattedTextField;
-    private JButton button1;
-    private JComboBox comboBox1;
+    private JButton loginButton;
+    private JComboBox userRoleComboBox;
     private JPasswordField passwordPasswordField;
     private JPanel mainPanel;
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel( new FlatIntelliJLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         Login loginScreen = new Login();
         loginScreen.setContentPane(loginScreen.mainPanel);
         loginScreen.setTitle("Pharma | Pharmacy POS");
