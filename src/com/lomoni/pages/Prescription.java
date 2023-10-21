8d82a73e4da1166402247aeb738530d8d4fe6ec7
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-10-21 20:45:18 +0300

    feature : Logging class events

--- src/com/lomoni/pages/Prescription.java
+++ src/com/lomoni/pages/Prescription.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import static com.lomoni.pages.utils.LogManagerImplementation.Log;
+
 public class Prescription {
     private JTable prescriptionTable;
     private JTextField rowFilterTextField;
@@ -23,31 +25,54 @@ public class Prescription {
     private JLabel lbl_error_login;
 
     public Prescription(PrescriptionService prescriptionService, TableFilter tableFilter){
-        setTableFilter(tableFilter);
-        setPrescriptionServiceData(prescriptionService);
-        setPlaceholderFunctionality();
+        try{
+            setTableFilter(tableFilter);
+            setPrescriptionServiceData(prescriptionService);
+            setPlaceholderFunctionality();
+            Log("INFO","Table filter set on prescription data",null);
+        }catch (Exception e){
+            Log("FATAL","Exception while setting table filter on Prescription data",e);
+        }
+
     }
 
     private void setTableFilter(TableFilter tableFilter){
-        tableFilter.setInventoryTable(prescriptionTable);
-        tableFilter.setFilterTextInput(rowFilterTextField);
-        rowFilterTextField.addKeyListener(tableFilter);
+        try{
+            tableFilter.setInventoryTable(prescriptionTable);
+            tableFilter.setFilterTextInput(rowFilterTextField);
+            rowFilterTextField.addKeyListener(tableFilter);
+            Log("INFO","Set table filter occured",null);
+        }catch(Exception e){
+            Log("FATAL","Exception occurred while setting the table filter : "+e.getMessage(),e);
+        }
+
     }
 
     private void setPrescriptionServiceData(PrescriptionService prescriptionService){
-        TableModel inventoryModel = new DefaultTableModel(prescriptionService.getInventoryDisplayData(), prescriptionService.getInventoryDisplayColumns());
-        prescriptionTable.setModel(inventoryModel);
+        try{
+            TableModel inventoryModel = new DefaultTableModel(prescriptionService.getInventoryDisplayData(), prescriptionService.getInventoryDisplayColumns());
+            prescriptionTable.setModel(inventoryModel);
+            Log("INFO", "Prescription data set",null);
+        }catch(Exception e){
+            Log("FATAL","Exception occurred while setting the prescription data : "+e.getMessage(),e);
+        }
     }
 
     private void setPlaceholderFunctionality(){
-        InputFieldFocusListener rowFilterTextFieldPlaceholder = new InputFieldFocusListener(rowFilterTextField, "Search...");
-        InputFieldFocusListener patientNamePlaceholder = new InputFieldFocusListener(patientName, "Patient Name");
-        InputFieldFocusListener dosagePlaceholder = new InputFieldFocusListener(strengthOfDosage, "Dosage ( e.g 500mg amoxicillin )");
-        InputFieldFocusListener frequencyPlaceholder = new InputFieldFocusListener(frequency, "Frequency ( e.g twice daily )");
-        rowFilterTextField.addFocusListener(rowFilterTextFieldPlaceholder);
-        patientName.addFocusListener(patientNamePlaceholder);
-        strengthOfDosage.addFocusListener(dosagePlaceholder);
-        frequency.addFocusListener(frequencyPlaceholder);
+        try{
+            InputFieldFocusListener rowFilterTextFieldPlaceholder = new InputFieldFocusListener(rowFilterTextField, "Search...");
+            InputFieldFocusListener patientNamePlaceholder = new InputFieldFocusListener(patientName, "Patient Name");
+            InputFieldFocusListener dosagePlaceholder = new InputFieldFocusListener(strengthOfDosage, "Dosage ( e.g 500mg amoxicillin )");
+            InputFieldFocusListener frequencyPlaceholder = new InputFieldFocusListener(frequency, "Frequency ( e.g twice daily )");
+            rowFilterTextField.addFocusListener(rowFilterTextFieldPlaceholder);
+            patientName.addFocusListener(patientNamePlaceholder);
+            strengthOfDosage.addFocusListener(dosagePlaceholder);
+            frequency.addFocusListener(frequencyPlaceholder);
+            Log("INFO", "Placeholders set",null);
+        }catch(Exception e){
+            Log("FATAL","Exception occurred while setting placeholders : "+e.getMessage(),e);
+        }
+
     }
 
     public JPanel createMainPanel(){
