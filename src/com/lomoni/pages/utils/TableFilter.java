1b7e56d5c9fe4134c01bea9907c2ef9dffa4c24d
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-10-21 19:02:07 +0300

    refactor : Some refactors.

--- src/com/lomoni/pages/utils/TableFilter.java
+++ src/com/lomoni/pages/utils/TableFilter.java
@@ -3,7 +3,14 @@ package com.lomoni.pages.utils;
 import javax.swing.*;
 import javax.swing.table.TableRowSorter;
 import java.awt.event.*;
+import static com.lomoni.pages.utils.LogManagerImplementation.Log;
 
+/*
+ * Author : Braine Lomoni 168864 21/10/2023
+ * Functionality :
+ * - Filtering JTable based on user input in JTextField
+ * - Applying RowFilter on JTable using TableRowSorter
+ */
 public class TableFilter implements KeyListener {
     private JTable inventoryTable;
 
@@ -16,17 +23,21 @@ public class TableFilter implements KeyListener {
         this.filterTextInput = filterTextInput;
     }
     private void filterTable(String filterText){
-        //TABLE ROW SORTER
-        TableRowSorter sorter = new TableRowSorter(inventoryTable.getModel());
+        try {
+            //TABLE ROW SORTER
+            TableRowSorter sorter = new TableRowSorter(inventoryTable.getModel());
 
-        //ROW FILTER
-        RowFilter rowFilter = RowFilter.regexFilter(filterText);
+            //ROW FILTER
+            RowFilter rowFilter = RowFilter.regexFilter(filterText);
 
-        //ROW FILTER ON TABLE ROW SORTER
-        sorter.setRowFilter(rowFilter);
+            //ROW FILTER ON TABLE ROW SORTER
+            sorter.setRowFilter(rowFilter);
 
-        //SET TABLE ROW SORTER ON THE TABLE
-        inventoryTable.setRowSorter(sorter);
+            //SET TABLE ROW SORTER ON THE TABLE
+            inventoryTable.setRowSorter(sorter);
+        } catch (Exception e){
+            Log("FATAL","Exception occurred while setting row filter on table: "+e.getMessage(),e);
+        }
     }
 
     @Override
